03ff754ce2cafc97fc30883cbabd0e9cdd5be552
Miguel Anjel Salazar Jaramillo <miguesalas2004@gmail.com>
2024-06-26 17:21:32 -0500

    Funciona el ruteo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./navbar/navbar.component";
 import { HeroComponent } from "./hero/hero.component";
 import { RegisterComponent } from "./register/register.component";
@@ -11,7 +11,7 @@ import { FooterComponent } from "./footer/footer.component";
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, NavbarComponent, HeroComponent, RegisterComponent, AdminPanelComponent, FooterComponent]
+    imports: [RouterOutlet, RouterLink]//, NavbarComponent, HeroComponent, RegisterComponent, AdminPanelComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'EstraMyPyme';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: NavbarComponent},
+    {path: 'navbar', component: NavbarComponent},
+    {path: 'register', component: RegisterComponent},
+    // {path: 'navbar', component: NavbarComponent}, 
+];
