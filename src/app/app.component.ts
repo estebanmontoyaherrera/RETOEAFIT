8d8a4c6bb9f17e2d61f8aefa5dc63a1c1f144a4d
Esteban Montoya Herrera <estebanmontoyaherrera7@gmail.com>
2024-06-25 19:06:55 -0500

    landing page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./navbar/navbar.component";
+import { HeroComponent } from "./hero/hero.component";
+import { RegisterComponent } from "./register/register.component";
+import { AdminPanelComponent } from "./admin-panel/admin-panel.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, NavbarComponent, HeroComponent, RegisterComponent, AdminPanelComponent, FooterComponent]
 })
 export class AppComponent {
-  title = 'FRONTEND';
+  title = 'EstraMyPyme';
 }
